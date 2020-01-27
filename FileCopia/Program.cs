38bcd47a1e2b11417fcd63e17f3c81151aa6c17a
ITT Rosselli Rasetti <rosselli.rasetti@outlook.com>
2020-01-27 09:52:28 +0100

    Add project files.

---
+++ FileCopia/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace FileCopia
+{
+    class Program
+    {
+        private const string file = "file.txt";
+        static void Main(string[] args)
+        {
+            using (StreamReader x = new StreamReader(file, Encoding.UTF8))
+            {
+
+            }
+        }
+    }
+}
